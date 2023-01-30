c3d3a943fe80e7ddd14179ece59fe3bf21a0fc78
Halisson Camargo <halissoncamargo.gestor@gmail.com>
2023-01-29 21:57:40 -0300

    Projetos secundários - removidos

--- 15 projetos/counter/app.js
+++
@@ -1,9 +0,0 @@
-const btnChange = document.querySelector(".btn");
-const colorCode = document.querySelector(".clrCode")
-
-btnChange.addEventListener('click', function(){
-   let num =   Math.floor(Math.random() * 10000000).toString(16).padStart(6,0);
-   document.body.style.backgroundColor = '#' + num;
-   colorCode.innerHTML = '#' + num;
-   
-})
